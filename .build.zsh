ae70ecbdb34f55daa80a7e95fe4695e38ccc590e
Jules Thivent <github@70k1.com>
2020-08-13 19:06:06 +0200

    Remove git pull command

--- .build.zsh
+++ .build.zsh
@@ -1,3 +1,3 @@
 # ZSH Script to run 'build' process on project
 #!/bin/zsh
-clear && cd && cd Documents/GitHub/portfolio3 && git pull && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby build
+clear && cd && cd Documents/GitHub/portfolio3 && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby build
