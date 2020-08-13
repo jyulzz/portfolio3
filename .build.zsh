a90ad14d2843f5c7c58ea99a68bf8fb1f4f3af3f
Jules Thivent <github@70k1.com>
2020-08-13 19:44:41 +0200

    Remove un-necessary path

--- .build.zsh
+++ .build.zsh
@@ -1,3 +1,3 @@
 # ZSH Script to run 'build' process on project
 #!/bin/zsh
-clear && cd && cd Documents/GitHub/portfolio3 && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby build
+clear && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby build
--- .develop.zsh
+++ .develop.zsh
@@ -1,3 +1,3 @@
 # ZSH Script to run 'develop' process on project
 #!/bin/zsh
-clear && cd && cd Documents/GitHub/portfolio3 && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby clean && gatsby develop
+clear && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby clean && gatsby develop
