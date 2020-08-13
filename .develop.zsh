f420ff2b79eac44d2a284c0585a3defa80829802
Jules Thivent <github@70k1.com>
2020-08-13 18:45:45 +0200

    Add ZSH scripts for 'build' and 'develop'

---
+++ .develop.zsh
@@ -0,0 +1,3 @@
+# ZSH Script to run 'develop' process on project
+#!/bin/zsh
+clear && cd && cd Documents/GitHub/portfolio3 && git pull && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby clean && gatsby develop
