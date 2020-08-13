f828df77212527b79464fd7627c5220426505e7c
Jules Thivent <github@70k1.com>
2020-08-13 20:46:49 +0200

    Improve format of ZSH scripts

--- .develop.zsh
+++ .develop.zsh
@@ -1,3 +1,12 @@
-# ZSH Script to run 'develop' process on project
+# ZSH Script to run 'gatsby develop' process on project
 #!/bin/zsh
-clear && npm install && prettier --write "**/*.{js,jsx,json,md,scss,yml}" && gatsby clean && gatsby develop
+clear
+echo "\n\033[1;32mdevelop.zsh: a ZSH Script to run the 'develop' process on this project.\033[0m"
+echo "\n\033[1;35m1. Install:\033[0m\n"
+npm install
+echo "\n\033[1;35m2. Prettier:\033[0m\n"
+prettier --write "**/*.{js,jsx,json,md,scss,yml}"
+echo "\n\033[1;35m3. Clean:\033[0m\n"
+gatsby clean
+echo "\n\033[1;35m4. Develop:\033[0m\n"
+gatsby develop
