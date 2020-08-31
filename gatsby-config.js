73c164e0e1a3cd651824c8d9d004300fb0701122
Jules Thivent <github@70k1.com>
2020-08-31 10:33:55 +0200

    Update CSP policies for script-src-elem (4)

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,10 @@ module.exports = {
         mergeStyleHashes: false,
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self' *.google-analytics.com *.cloudfront.net",
+          "script-src":
+            "'self' data: 'unsafe-inline' *.google-analytics.com *.cloudfront.net",
           "script-src-elem":
-            "'self' data: 'unsafe-inline' *.google-analytics.com",
+            "'self' data: 'unsafe-inline' *.google-analytics.com *.cloudfront.net",
           "font-src": "data: 'self' fonts.gstatic.com *.cloudfront.net",
           "style-src":
             "data: 'unsafe-inline' 'self' *.googleapis.com *.cloudfront.net",
