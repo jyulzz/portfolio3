70ad4dd263c28822a41b9dce2830794aea13bd77
Jules Thivent <github@70k1.com>
2021-05-10 21:36:10 +0200

    Add vulnerability auditing and fixing

--- .build.zsh
+++ .build.zsh
@@ -13,10 +13,14 @@ sudo clear
 echo "\n\033[1;32mbuild.zsh: a ZSH Script to run 'gatsby build' on this project.\033[0m"
 echo "\n\033[1;35m1. Install:\033[0m\n"
 npm install
-echo "\n\033[1;35m2. Clean:\033[0m\n"
+echo "\n\033[1;35m2. Audit Vulnerabilities:\033[0m\n"
+npm audit
+echo "\n\033[1;35m3. Fix Vulnerabilities:\033[0m\n"
+npm audit fix
+echo "\n\033[1;35m4. Clean:\033[0m\n"
 sudo gatsby clean
-echo "\n\033[1;35m3. Prettier:\033[0m\n"
+echo "\n\033[1;35m5. Prettier:\033[0m\n"
 sudo prettier --write "**/*.{js,jsx,json,scss,yml}"
-echo "\n\033[1;35m4. Build:\033[0m\n"
+echo "\n\033[1;35m6. Build:\033[0m\n"
 sudo gatsby build
 echo "\n\033[1;35mBuild: DONE\033[0m\n"
--- .develop.zsh
+++ .develop.zsh
@@ -13,9 +13,13 @@ sudo clear
 echo "\n\033[1;32mdevelop.zsh: a ZSH Script to run 'gatsby develop' on this project.\033[0m"
 echo "\n\033[1;35m1. Install:\033[0m\n"
 npm install
-echo "\n\033[1;35m2. Clean:\033[0m\n"
+echo "\n\033[1;35m2. Audit Vulnerabilities:\033[0m\n"
+npm audit
+echo "\n\033[1;35m3. Fix Vulnerabilities:\033[0m\n"
+npm audit fix
+echo "\n\033[1;35m4. Clean:\033[0m\n"
 sudo gatsby clean
-echo "\n\033[1;35m3. Prettier:\033[0m\n"
+echo "\n\033[1;35m5. Prettier:\033[0m\n"
 prettier --write "**/*.{js,jsx,json,scss,yml}"
-echo "\n\033[1;35m4. Develop:\033[0m\n"
+echo "\n\033[1;35m6. Develop:\033[0m\n"
 sudo gatsby develop
