f9124578ea35f69bfa50a9ec7ad1f7e3fdba7e70
Jules Thivent <github@70k1.com>
2021-05-14 10:40:37 +0200

    Move rendering options for Hero component to dedicated file

--- src/components/hero.js
+++ src/components/hero.js
@@ -14,9 +14,8 @@ Hero section used on the Index page.
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import Emoji from "a11y-react-emoji";
+import Options from "../options/about.js";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 import { Container } from "../components/grid";
 import Section from "../components/section";
 /*-----------------------------------------------------------------------------*
@@ -24,49 +23,10 @@ import Section from "../components/section";
 *-----------------------------------------------------------------------------*/
 
 /*-----------------------------------------------------------------------------*
-  OPTIONS
-*-----------------------------------------------------------------------------*/
-/* 'options' contains rendering directives for Rich Text content received from Contentful, including embedded assets, paragraphs and headers. */
-const options = {
-  renderNode: {
-    [BLOCKS.PARAGRAPH]: (node, children) => {
-      return <p>{children}</p>;
-    },
-    [BLOCKS.HEADING_1]: (node, children) => {
-      return (
-        <h1>
-          <Emoji symbol="👋" label="Waiving Hand Emoji" /> {children}
-        </h1>
-      );
-    },
-    [MARKS.BOLD]: (node, children) => {
-      return <b>{children}</b>;
-    },
-    [BLOCKS.HEADING_2]: (node, children) => {
-      return <h2>{children}</h2>;
-    },
-    [BLOCKS.HEADING_3]: (node, children) => {
-      return <h3>{children}</h3>;
-    },
-    [BLOCKS.HEADING_4]: (node, children) => {
-      return <h4>{children}</h4>;
-    },
-    [BLOCKS.HEADING_5]: (node, children) => {
-      return <h5>{children}</h5>;
-    },
-    [BLOCKS.HEADING_6]: (node, children) => {
-      return <h6>{children}</h6>;
-    },
-  },
-};
-/*-----------------------------------------------------------------------------*
-  /OPTIONS
-*-----------------------------------------------------------------------------*/
-
-/*-----------------------------------------------------------------------------*
   COMPONENTS
 *-----------------------------------------------------------------------------*/
-const Hero = ({ data }) => {
+const Hero = ({ data = {}, options = {} }) => {
+  options = Options;
   /* Pull the Site entry from Contentful for this site. */
   data = useStaticQuery(graphql`
     {
@@ -99,9 +59,11 @@ const Hero = ({ data }) => {
 *-----------------------------------------------------------------------------*/
 Hero.propTypes = {
   data: PropTypes.object.isRequired,
+  options: PropTypes.object.isRequired,
 };
 Hero.defaultProps = {
   data: {},
+  options: {},
 };
 /*-----------------------------------------------------------------------------*
   /PROPS
---
+++ src/options/hero.js
@@ -0,0 +1,68 @@
+/*-----------------------------------------------------------------------------*
+
+FILE
+src/options/about.js
+
+DESCRIPTION
+Render options for the Hero section for the Index page.
+
+*-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------*
+  IMPORTS
+*-----------------------------------------------------------------------------*/
+import React from "react";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import Emoji from "a11y-react-emoji";
+
+/*-----------------------------------------------------------------------------*
+  /IMPORTS
+*-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------*
+  OPTIONS
+*-----------------------------------------------------------------------------*/
+/* 'options' contains rendering directives for Rich Text content received from Contentful, including embedded assets, paragraphs and headers. */
+const Options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      return <p>{children}</p>;
+    },
+    [BLOCKS.HEADING_1]: (node, children) => {
+      return (
+        <h1>
+          <Emoji symbol="👋" label="Waiving Hand Emoji" /> {children}
+        </h1>
+      );
+    },
+    [MARKS.BOLD]: (node, children) => {
+      return <b>{children}</b>;
+    },
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return <h2>{children}</h2>;
+    },
+    [BLOCKS.HEADING_3]: (node, children) => {
+      return <h3>{children}</h3>;
+    },
+    [BLOCKS.HEADING_4]: (node, children) => {
+      return <h4>{children}</h4>;
+    },
+    [BLOCKS.HEADING_5]: (node, children) => {
+      return <h5>{children}</h5>;
+    },
+    [BLOCKS.HEADING_6]: (node, children) => {
+      return <h6>{children}</h6>;
+    },
+  },
+};
+/*-----------------------------------------------------------------------------*
+  /OPTIONS
+*-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------*
+  EXPORTS
+*-----------------------------------------------------------------------------*/
+export default Options;
+/*-----------------------------------------------------------------------------*
+  /EXPORTS
+*-----------------------------------------------------------------------------*/
